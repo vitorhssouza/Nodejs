fe4f35ac96acb905ea8ed9893041cae315573e61
vitor souza <vitorugo_kta@hotmail.com>
2022-11-08 21:15:13 -0300

    Outra forma de definir o modulo interno

--- 01_Modulos_Internos/meu_modulo.js
+++ 01_Modulos_Internos/meu_modulo.js
@@ -1,17 +1,20 @@
 module.exports = {
-
-    soma(num1, num2){
-        return num1 + num2;
-    },    
-    subtracao(num1, num2){
-        return num1 - num2;
-    },    
-    multiplicacao(num1, num2){
-        return num1 * num2;
-    },
-    divisao(num1, num2){
-        return num1 / num2;
-    }
+    soma,
+    subtracao,
+    multiplicacao,
+    divisao    
 
 }
 
+function soma(num1, num2){
+    return num1 + num2;
+}    
+function subtracao(num1, num2){
+    return num1 - num2;
+} 
+function multiplicacao(num1, num2){
+    return num1 * num2;
+}
+function divisao(num1, num2){
+    return num1 / num2;
+}
