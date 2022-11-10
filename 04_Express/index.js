faf5fa7450d02263706d79278097c6c91b7dc24b
vitor souza <vitorugo_kta@hotmail.com>
2022-11-09 21:15:47 -0300

    rederizando arquivo html

--- 04_Express/index.js
+++ 04_Express/index.js
@@ -1,10 +1,16 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const porta = 3000; // Endereço para acesso ao servidor
+const basePath = path.join(__dirname, 'templates')
+
 
 app.get('/', (requisicao, resposta) => {
     // enviando essa resposta
-    resposta.send('Está é minha primeira página utilizando o node.js')
+    //resposta.send('Está é minha primeira página utilizando o node.js')
+
+    // Enviando um arquivo html
+    resposta.sendFile(`${basePath}/index.html`);
 });
 
 
