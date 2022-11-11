faa525e1ca39662b647e92a288af65f77aca0a30
vitor souza <vitorugo_kta@hotmail.com>
2022-11-10 23:01:02 -0300

    atualizacao

--- 03_Modulo_externo/connect.js
+++ 03_Modulo_externo/connect.js
@@ -3,7 +3,7 @@ var mysql  = require('mysql2');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Ci&nci4d&d4d0s2008',
+  password : 'xxxxxx',
   database : 'db_senai'
 });
  
